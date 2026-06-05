a1764ede713bc8b95349600d61a2c35b3ec4ae10
Pranav24PW-25 <pkpranavirs@gmail.com>
2026-06-05 20:58:32 +0530

    feat: complete week 1 pipeline architecture setup with fixed gitignore

---
+++ pipeline.py
@@ -0,0 +1,40 @@
+import torch
+from torch.utils.data import Dataset,DataLoader
+
+#This line checks if you have a GPU else safely falls back to CPU
+
+device = torch.device("cuda" if torch.cuda.is_available() else"cpu")
+print(f"Factory Power Source Selected: {device}")
+
+class ReviewDataset(Dataset):
+    def __init__(self,reviews,labels):
+        #reviews:A list of strings
+        #lables:A list of number(0 for negative,1 for positive)
+
+        self.reviews = reviews
+        self.labels = labels
+    
+    def __len__(self):
+        return len(self.reviews)
+    def __getitem__(self,idx):
+        single_review = self.reviews[idx]
+        single_label = self.labels[idx]
+
+        return single_review,single_label
+    
+mock_reviews = [
+    "This movie was absolutely amazing!",
+    "I hated this product, it broke instantly.",
+    "Decent quality, but shipping took too long.",
+    "Best purchase I have made all year."
+]
+mock_labels = [1, 0, 0, 1]
+
+dataset = ReviewDataset(mock_reviews,mock_labels)
+dataLoader = DataLoader(dataset,batch_size = 2,shuffle = True)
+
+print("\n--- Testing Conveyor Belt (DataLoader) Batches ---")
+for batch_idx, (batch_reviews, batch_labels) in enumerate(dataLoader):
+    print(f"Box {batch_idx + 1} coming down the belt:")
+    print(f"  Processed Reviews: {batch_reviews}")
+    print(f"  Processed Labels:  {batch_labels}")
